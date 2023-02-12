8c28e7d868b0570d9b31a1c3ebece7d292c865f3
workload2022 <mohosin.csm@gmail.com>
2023-02-12 11:26:53 +0600

    Movie List Added

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): Response
+    #[Route('/movies', name: 'movie_list')]
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $movies = $doctrine->getRepository(Movie::class)->findAll();
+// dd($movies);
         return $this->render('movies/index.html.twig', [
-            'controller_name' => 'MoviesController',
+            'movies' => $movies,
         ]);
     }
 
